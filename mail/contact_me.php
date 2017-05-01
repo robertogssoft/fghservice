8f98d2fb01ecfe25bc2363fd327f846e68461d19
robertogs <robertogs.soft@gmail.com>
2017-04-30 19:55:50 -0500

    cambiando remitente para formulario de contacto

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,7 +17,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = 'fghmxdeveloper@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website:  $name";
 $email_body = "Hemos recibido un mensaje de:.\n\n"."Detalles:\n\nName: $name\n\nCorreo: $email_address\n\nMessage:\n$message";
-$headers = "From: fghmxdeveloper@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@fghservice.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
